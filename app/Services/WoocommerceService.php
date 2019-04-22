e16db0687a060a6b270e5a6b04b7a14ae41d95aa
Jhonnel Rosales <jhonnel@bbo.com.ph>
2019-04-22 23:52:01 +0800

    fixed

--- app/Services/WoocommerceService.php
+++ app/Services/WoocommerceService.php
@@ -16,6 +16,7 @@ class WoocommerceService
 		    [
 		        'wp_api' => config('woocommerce.wp_api'),
 		        'version' => config('woocommerce.api_version'),
+		        'query_string_auth' => config('woocommerce.query_string_auth'),
 		    ]
 		);
 	}
@@ -49,46 +50,46 @@ class WoocommerceService
 
 	public function getCategories()
 	{
-		return DB::select("SELECT bbo_terms.* FROM bbo_terms LEFT JOIN bbo_term_taxonomy ON(bbo_terms.term_id = bbo_term_taxonomy.term_id) LEFT JOIN bbo_posts ON(bbo_posts.ID = bbo_terms.term_id) WHERE bbo_term_taxonomy.taxonomy = 'product_cat'");
+		return DB::select("SELECT wpla_terms.* FROM wpla_terms LEFT JOIN wpla_term_taxonomy ON(wpla_terms.term_id = wpla_term_taxonomy.term_id) LEFT JOIN wpla_posts ON(wpla_posts.ID = wpla_terms.term_id) WHERE wpla_term_taxonomy.taxonomy = 'product_cat'");
 	}
 
 	public function getBrands()
 	{
-		return DB::select("SELECT bbo_terms.* FROM bbo_terms LEFT JOIN bbo_term_taxonomy ON(bbo_terms.term_id = bbo_term_taxonomy.term_id) WHERE bbo_term_taxonomy.taxonomy = 'yith_product_brand'");
+		return DB::select("SELECT wpla_terms.* FROM wpla_terms LEFT JOIN wpla_term_taxonomy ON(wpla_terms.term_id = wpla_term_taxonomy.term_id) WHERE wpla_term_taxonomy.taxonomy = 'yith_product_brand'");
 	}
 
 	public function getProducts()
 	{
-		return DB::select("SELECT DISTINCT wposts.*, wm2.meta_value as images, wm3.meta_value as regular_price, wm4.meta_value as sale_price, wm4.meta_value as price, wpo.option_value as siteurl FROM bbo_posts wposts JOIN bbo_options wpo ON wpo.option_name = 'siteurl' LEFT JOIN bbo_postmeta wm1 ON (wposts.ID = wm1.post_id AND wm1.meta_value IS NOT NULL AND wm1.meta_key = '_thumbnail_id') LEFT JOIN bbo_postmeta wm2 ON (wm1.meta_value = wm2.post_id AND wm2.meta_key = '_wp_attachment_metadata' AND wm2.meta_value IS NOT NULL) LEFT JOIN bbo_postmeta wm3 ON (wposts.ID = wm3.post_id AND wm3.meta_key = '_regular_price' AND wm3.meta_value IS NOT NULL) LEFT JOIN bbo_postmeta wm4 ON (wposts.ID = wm4.post_id AND wm4.meta_key = '_sale_price' AND wm4.meta_value IS NOT NULL) LEFT JOIN bbo_postmeta wm5 ON (wposts.ID = wm5.post_id AND wm5.meta_key = '_price' AND wm5.meta_value IS NOT NULL) LEFT JOIN bbo_term_relationships ON (wposts.ID = bbo_term_relationships.object_id) LEFT JOIN bbo_term_taxonomy ON (bbo_term_relationships.term_taxonomy_id = bbo_term_taxonomy.term_taxonomy_id) WHERE wposts.post_type = 'product' AND wposts.post_status = 'publish'");
+		return DB::select("SELECT DISTINCT wposts.*, wm2.meta_value as images, wm3.meta_value as regular_price, wm4.meta_value as sale_price, wm4.meta_value as price, wpo.option_value as siteurl FROM wpla_posts wposts JOIN wpla_options wpo ON wpo.option_name = 'siteurl' LEFT JOIN wpla_postmeta wm1 ON (wposts.ID = wm1.post_id AND wm1.meta_value IS NOT NULL AND wm1.meta_key = '_thumbnail_id') LEFT JOIN wpla_postmeta wm2 ON (wm1.meta_value = wm2.post_id AND wm2.meta_key = '_wp_attachment_metadata' AND wm2.meta_value IS NOT NULL) LEFT JOIN wpla_postmeta wm3 ON (wposts.ID = wm3.post_id AND wm3.meta_key = '_regular_price' AND wm3.meta_value IS NOT NULL) LEFT JOIN wpla_postmeta wm4 ON (wposts.ID = wm4.post_id AND wm4.meta_key = '_sale_price' AND wm4.meta_value IS NOT NULL) LEFT JOIN wpla_postmeta wm5 ON (wposts.ID = wm5.post_id AND wm5.meta_key = '_price' AND wm5.meta_value IS NOT NULL) LEFT JOIN wpla_term_relationships ON (wposts.ID = wpla_term_relationships.object_id) LEFT JOIN wpla_term_taxonomy ON (wpla_term_relationships.term_taxonomy_id = wpla_term_taxonomy.term_taxonomy_id) WHERE wposts.post_type = 'product' AND wposts.post_status = 'publish'");
 	}
 
 	public function getProductCategories($productId)
 	{
-		return DB::select("SELECT DISTINCT bbo_terms.* FROM bbo_posts LEFT JOIN bbo_term_relationships ON(bbo_posts.ID = bbo_term_relationships.object_id) LEFT JOIN bbo_term_taxonomy ON(bbo_term_relationships.term_taxonomy_id = bbo_term_taxonomy.term_taxonomy_id) LEFT JOIN bbo_terms ON(bbo_term_taxonomy.term_id = bbo_terms.term_id) WHERE bbo_term_taxonomy.taxonomy = 'product_cat' AND bbo_posts.ID =". $productId);
+		return DB::select("SELECT DISTINCT wpla_terms.* FROM wpla_posts LEFT JOIN wpla_term_relationships ON(wpla_posts.ID = wpla_term_relationships.object_id) LEFT JOIN wpla_term_taxonomy ON(wpla_term_relationships.term_taxonomy_id = wpla_term_taxonomy.term_taxonomy_id) LEFT JOIN wpla_terms ON(wpla_term_taxonomy.term_id = wpla_terms.term_id) WHERE wpla_term_taxonomy.taxonomy = 'product_cat' AND wpla_posts.ID =". $productId);
 	}
 
 	public function getProductBrands($productId)
 	{
-		return DB::select("SELECT DISTINCT bbo_terms.* FROM bbo_posts LEFT JOIN bbo_term_relationships ON(bbo_posts.ID = bbo_term_relationships.object_id) LEFT JOIN bbo_term_taxonomy ON(bbo_term_relationships.term_taxonomy_id = bbo_term_taxonomy.term_taxonomy_id) LEFT JOIN bbo_terms ON(bbo_term_taxonomy.term_id = bbo_terms.term_id) WHERE bbo_term_taxonomy.taxonomy = 'yith_product_brand' AND bbo_posts.ID =". $productId);
+		return DB::select("SELECT DISTINCT wpla_terms.* FROM wpla_posts LEFT JOIN wpla_term_relationships ON(wpla_posts.ID = wpla_term_relationships.object_id) LEFT JOIN wpla_term_taxonomy ON(wpla_term_relationships.term_taxonomy_id = wpla_term_taxonomy.term_taxonomy_id) LEFT JOIN wpla_terms ON(wpla_term_taxonomy.term_id = wpla_terms.term_id) WHERE wpla_term_taxonomy.taxonomy = 'yith_product_brand' AND wpla_posts.ID =". $productId);
 	}
 
 	public function updateProductBrand($productId, $currentBrandId, $brandId)
 	{
-		return DB::select("UPDATE bbo_term_relationships SET term_taxonomy_id='$brandId' WHERE object_id = '$productId' AND term_taxonomy_id= '$currentBrandId'");
+		return DB::select("UPDATE wpla_term_relationships SET term_taxonomy_id='$brandId' WHERE object_id = '$productId' AND term_taxonomy_id= '$currentBrandId'");
 	}
 
 	public function createProductBrand($productId, $brandId)
 	{
-		return DB::insert("INSERT INTO bbo_term_relationships (object_id, term_taxonomy_id,term_order) values (?, ?, ?)", [$productId, $brandId, 0]);
+		return DB::insert("INSERT INTO wpla_term_relationships (object_id, term_taxonomy_id,term_order) values (?, ?, ?)", [$productId, $brandId, 0]);
 	}
 
 	public function createBrand($name, $slug, $taxonomy)
 	{
-		$inserted = DB::insert("INSERT INTO bbo_terms (name, slug,term_group) values (?, ?, ?)", [$name, $slug, 0]);
+		$inserted = DB::insert("INSERT INTO wpla_terms (name, slug,term_group) values (?, ?, ?)", [$name, $slug, 0]);
 		
 		if ($inserted) {
 			$id = DB::getPdo()->lastInsertId();
-			return DB::insert("INSERT INTO bbo_term_taxonomy (term_id, taxonomy,description, parent, count) values (?, ?, ?, ?, ?)", [$id, $taxonomy, '', 0, 0]);
+			return DB::insert("INSERT INTO wpla_term_taxonomy (term_id, taxonomy,description, parent, count) values (?, ?, ?, ?, ?)", [$id, $taxonomy, '', 0, 0]);
 		}
 
 		return false;
@@ -96,15 +97,15 @@ class WoocommerceService
 
 	public function updateBrand($brandId, $name, $slug)
 	{
-		return DB::update("UPDATE bbo_terms set name = '$name', slug='$name' where term_id = ?", [$brandId]);
+		return DB::update("UPDATE wpla_terms set name = '$name', slug='$name' where term_id = ?", [$brandId]);
 	}
 
 	public function deleteBrand($brandId)
 	{
-		$deleted = DB::delete("DELETE FROM bbo_terms WHERE term_id =?", [$brandId]);
+		$deleted = DB::delete("DELETE FROM wpla_terms WHERE term_id =?", [$brandId]);
 
 		if ($deleted) {
-			return DB::delete("DELETE FROM bbo_term_taxonomy WHERE term_id =?", [$brandId]);
+			return DB::delete("DELETE FROM wpla_term_taxonomy WHERE term_id =?", [$brandId]);
 		}
 
 		return false;
@@ -112,6 +113,6 @@ class WoocommerceService
 
 	public function getBrand($brandId)
 	{
-		return DB::select("SELECT * FROM bbo_terms WHERE term_id=". $brandId);
+		return DB::select("SELECT * FROM wpla_terms WHERE term_id=". $brandId);
 	}
 }	
