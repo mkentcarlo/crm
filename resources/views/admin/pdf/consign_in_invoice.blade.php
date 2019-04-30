2e7207ed753c074c0a95d19eedc2468e53ae906f
Kent Carlo <melquiadeskentcarlo@gmail.com>
2019-05-01 01:56:28 +0800

    added layout for consignment in pdf

--- resources/views/admin/pdf/consign_in_invoice.blade.php
+++ resources/views/admin/pdf/consign_in_invoice.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Repair Invoice</title>
+	<title>Consign In Invoice</title>
 	<!-- Custom CSS -->
 	<style>
 		.logo {
@@ -18,14 +18,23 @@
 		}
 		@import url({{ asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css') }});
 		
-		table td,
-		table th {
+		table  {
+			width: 100%;
+		}
+
+		table.bordered td,
+		table.bordered th {
 			border: 1px solid #000;
 		}
 		table th {
 			background: #d9d9d9;
 			text-transform: uppercase;
 			font-weight: normal;
+		}
+		.underline {
+			display: inline-block;
+			width: 300px;
+			border-bottom: 2px solid #000;
 		}
 	</style>
 </head>
@@ -42,15 +51,15 @@ Tel: +65 6388 5555 | +65 8715 5555 | Email: service@luxemontre.sg</small></p>
 		</div>
 		<div class="row">
 			<div class="col-md-12 text-right">
-				<h5>SERVICE AND REPAIR SLIP</h5>
+				<h5>CONSIGNMENT INVOICE</h5>
 				<h6>UEN NO: 230215K</h6>
-				<h6>SERVICE NO.: <span style="color: red"><strong>0001</strong></span></h6>
+				<h6>CI (IN) NO.: <span style="color: red"><strong>0001</strong></span></h6>
 			</div>
 		</div>
-		<table style="width: 100%">
+		<table style="width: 100%" class="bordered">
 			<thead>
 				<tr>
-					<td>Client: </td>
+					<td>Consignor: </td>
 					<td>Phone: </td>
 				</tr>
 				<tr>
@@ -59,138 +68,100 @@ Tel: +65 6388 5555 | +65 8715 5555 | Email: service@luxemontre.sg</small></p>
 				</tr>
 				<tr>
 					
+					<td>NIRC/PASSPORT NO: </td>
 					<td>Date: </td>
 				</tr>
 			</thead>
-			<thead>
-				<tr>
-					<th colspan="2" class="text-center">Watch Description</th>
-				</tr>
-			</thead>
 			<tbody>
 				<tr>
-					<td>Brand: </td>
-					<td>Strap Type: {{ $invoice->additional_fields->strap_type ?? null }}</td>
+					<td colspan="2">Included: </td>
+				</tr>
+				<tr>
+					<td colspan="2">Watch Condition: </td>
+				</tr>
+				<tr>
+					<td>Remarks: </td>
+					<td>Bracelet Conditions/Links: </td>
 				</tr>
 				</tr>
 				<tr>
-					<td>Model No: </td>
-					<td>Case Material: </td>
+					<td>Consignment Period: </td>
+					<td>Return Date: </td>
 				</tr>
-				<tr>
-					<td>Serial No.:  </td>
-					<td>Within Warranty Period: </td>
-				</tr>
-				<tr>
-					<td>Movement: </td>
-					<td>Warranty Attached: </td>
-				</tr>
-				<tr>
-					<td>Functions: </td>
-					<td>Bracelet Condition/Links: </td>
-				</tr>
-			</tbody>
-			<thead>
-				<tr>
-					<th colspan="2" class="text-center">Condition of Watch for Repair</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td colspan="2">Complete Case: </td>
-				</tr>
-				<tr>
-					<td>Glass/Sapphire Crystal: </td>
-					<td>Case Back: </td>
-				</tr>
-				<tr>
-					<td>Dial: </td>
-					<td>Hands: </td>
-				</tr>
-				<tr>
-					<td>Bezel: </td>
-					<td>Crown/Pushers: {{ $invoice->additional_fields->crown ?? null }}</td>
-				</tr>
-				<tr>
-					<td>Bracelet/Strap: </td>
-					<td>Clasp/Buckle: </td>
-				</tr>
-			</tbody>
-			<thead>
-				<tr>
-					<th colspan="2" class="text-center">Watch Service / Repair Information</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Timing: </td>
-					<td>Complete Service: </td>
-				</tr>
-				<tr>
-					<td>Power Reserve: </td>
-					<td>Polish/Refurbishing: </td>
-				</tr>
-				<tr>
-					<td>Keeps Stopping:  </td>
-					<td>Checking/Monitoring: </td>
-				</tr>
-				<tr>
-					<td>Water Seepage: </td>
-					<td>Battery Change: </td>
-				</tr>
-				<tr>
-					<td>Parts Broken: </td>
-					<td>Glass/Crystal Change: </td>
-				</tr>
-				<tr>
-					<td>Parts Missing: </td>
-					<td>Strap Change: </td>
-				</tr>
-			</tbody>
-			<thead>
-				<tr>
-					<th colspan="2" class="text-center">Service / Repair Quotation for Internal Use</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Service/Repair Cost: </td>
-					<td>Service/Repair Accepted: </td>
-				</tr>
-				<tr>
-					<td>Service/Repair Duration <small>(estimate)</small>: </td>
-					<td>Date of Acceptance: </td>
-				</tr>
-			</tbody>
 			
 		</table>
-		<table style="width: 100%">
+		<table style="width: 100%;" class="bordered">
 			<thead>
 				<tr>
-					<th class="text-center" colspan="4">Authorization for dispatch and collection of watch</th>
+					<td class="text-center">ITEM</td>
+					<td class="text-center">DDSCRIPTION (MODEL & SERIAL NO.)</td>
+					<td class="text-center">AMOUNT (SGD)</td>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
-					<td><strong>Watch Accepted By</strong>:<br> <small style="font-size: 11px !important;">On behalf of Luxe Montre/Date</small> <br> </td>
 					<td>&nbsp;</td>
-					<td><strong>Watch Accepted By</strong>:<br> <small style="font-size: 11px !important;">On behalf of Luxe Montre/Date</small> <br> </td>
 					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td><strong>Watch Accepted By</strong>:<br> <small style="font-size: 11px !important;">On behalf of Luxe Montre/Date</small> <br> </td>
-					<td>&nbsp;</td>
-					<td><strong>Watch Accepted By</strong>:<br> <small style="font-size: 11px !important;">On behalf of Luxe Montre/Date</small> <br> </td>
 					<td>&nbsp;</td>
 				</tr>
+			<tbody>
 				<tr>
-					<td style="font-size: 10px;" colspan="4">
-					<ul style="font-size: 10px; margin: 0; padding-left: 15px;line-height: 15px;"><li style="font-size: 10px;">The Original Copy of this Service And Repair Slip MUST be presented upon the collection of watch. Please contact us in writing if it is misplaced or lost. </li> <br>
-<li style="font-size: 10px;">In the absence of the Original Copy of this Service And Repair Slip, a Letter of Authorization from the abovementioned client and NRIC is REQUIRED.</li>
-					</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+			<tbody>
+				<tr>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+			<tbody>
+				<tr>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+			<tbody>
+				<tr>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+			<tbody>
+				<tr>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
 				</tr>
 			</tbody>
 		</table>
-	</div>
+		<br>
+		<p><strong>I agree to consign the above product(s) to Luxe Montre Pte. Ltd. on the following Terms and Conditions:</strong></p>
+		<ul>
+			<li>The product(s) are not counterfeit, modified and/or stolen property.</li>
+			<li>The product(s) belong to me.</li>
+			<li>I hereby grant Luxe Montre Pte. Ltd. the exclusive right to market/sell my product(s) for a 60 day period.</li>
+			<li>I have read and concur with the General Consignment Terms and Agreement by Luxe Montre Pte. Ltd.</li>
+			<li>The Original Copy of this Consignment Invoice MUST be presented upon collection of sales payment, withdrawal or termination.</li>
+		</ul>
+			
+			<br>
+			<br>
+			<br>
+		<table>
+			<tbody>
+				<tr>
+					<td><span class="underline">
+				</span>
+				<br>
+				For and on behalf of <strong>Luxe Montre Pte. Ltd.</strong></td>
+					<td><span class="underline">
+				</span>
+				<br>
+				<strong>Consignor Signature</strong></td>
+				</tr>
+			</tbody>	
+		</table>
 </body>
 </html>
