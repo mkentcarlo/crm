383d8493ab07d8386ddea8a7c698daffd2d062fd
Kent Carlo <melquiadeskentcarlo@gmail.com>
2019-05-03 15:24:54 +0800

    fix

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -16,7 +16,7 @@
     <div id="mobile_only_nav" class="mobile-only-nav pull-right">
         <ul class="nav navbar-right top-nav pull-right">
             <li class="dropdown auth-drp">
-                <a href="#" class="dropdown-toggle pr-0" data-toggle="dropdown"><img src="/img/user1.png" alt="user_auth" class="user-auth-img img-circle"/><span class="user-online-status"></span></a>
+                <a href="#" class="dropdown-toggle pr-0" data-toggle="dropdown"><img src="{{asset('img/user1.png')}}" alt="user_auth" class="user-auth-img img-circle"/><span class="user-online-status"></span></a>
                 <ul class="dropdown-menu user-auth-dropdown" data-dropdown-in="flipInX" data-dropdown-out="flipOutX">
                     <li>
                         <a href="{{ url('/users/profile/'. auth()->user()->id) }}"><i class="zmdi zmdi-account"></i><span>Edit Information</span></a>
