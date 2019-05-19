349e84260442e5f04257579fc5597caa6f1ad9f0
Jhonnel Rosales <jhonnel@bbo.com.ph>
2019-05-19 17:31:50 +0800

    update sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -22,6 +22,7 @@
             </ul>
         </li>
         @endcan
+        @can('view.customer')
         <li>
             <a class="{{in_array(Route::current()->getName(), array('view.customer', 'view.group')) ? 'active' : ''}}" href="javascript:void(0);" data-toggle="collapse" data-target="#cust_dr"  class=""><div class="pull-left"><i class="fa fa-users mr-20"></i><span class="right-nav-text">Customers</span></div><div class="pull-right"><i class="zmdi zmdi-caret-down"></i></div><div class="clearfix"></div></a>
             <ul id="cust_dr" class="collapse collapse-level-1 {{in_array(Route::current()->getName(), array('view.customer', 'view.group')) ? 'in' : ''}}">
@@ -33,26 +34,34 @@
                 </li>
             </ul>
         </li>
+        @endcan
         <li>
             <a href="{{ route('view.inquiries') }}" class="{{Route::current()->getName() == 'view.inquiries' ? 'active' : ''}}"><div class="pull-left"><i class="fa fa-envelope-square mr-20" aria-hidden="true"></i><span class="right-nav-text">Enquiries </span></div><div class="clearfix"></div></a>
         </li>
         <li>
             <a class="{{in_array(Route::current()->getName(), array('view.product','create.product','view.brand', 'view.category')) ? 'active' : ''}}" href="javascript:void(0);" data-toggle="collapse" data-target="#app_dr"><div class="pull-left"><i class="zmdi zmdi-time mr-20"></i><span class="right-nav-text">Watches </span></div><div class="pull-right"><i class="zmdi zmdi-caret-down"></i></div><div class="clearfix"></div></a>
             <ul id="app_dr" class="collapse collapse-level-1 {{in_array(Route::current()->getName(), array('view.product','create.product','view.brand', 'view.category')) ? 'in' : ''}}">
+                @can('view.product')
                 <li>
                     <a class="{{Route::current()->getName() == 'view.product' ? 'active-page' : ''}}" href="{{ route('view.product') }}" class="">List</a>
                 </li>
                 <li>
                     <a class="{{Route::current()->getName() == 'create.product' ? 'active-page' : ''}}" href="{{ route('create.product') }}">Add Watch</a>
                 </li>
+                @endcan
+                @can('view.brand')
                 <li>
                     <a class="{{Route::current()->getName() == 'view.brand' ? 'active-page' : ''}}" href="{{ route('view.brand') }}" class="">brands</a>
                 </li>
+                @endcan
+                @can('view.category')
                 <li>
                     <a class="{{Route::current()->getName() == 'view.category' ? 'active-page' : ''}}" href="{{ route('view.category') }}" class="">categories</a>
                 </li>
+                @endcan
             </ul>
         </li>
+        @can('view.invoice')
         <li>
             <a class="{{Route::current()->getName() == 'view.invoice' ? 'active' : ''}}" href="javascript:void(0);" data-toggle="collapse" data-target="#invoicemgt" class=""><div class="pull-left"><i class="fa fa-file-text-o mr-20" aria-hidden="true"></i><span class="right-nav-text">Invoice Management </span></div><div class="pull-right"><i class="zmdi zmdi-caret-down"></i></div><div class="clearfix"></div></a>
             <ul id="invoicemgt" class="collapse collapse-level-1 {{Route::current()->getName() == 'view.invoice' ? 'in' : ''}}">
@@ -76,6 +85,7 @@
                 </li>
             </ul>
         </li>
+        @endcan
         <li>
             <!-- <a href="{{ route('view.report') }}" class=""><div class="pull-left"><i class="fa fa-bar-chart-o mr-20" aria-hidden="true"></i><span class="right-nav-text">Reports </span></div><div class="clearfix"></div></a> -->
              <a href="javascript:void(0);" data-toggle="collapse" data-target="#reportmgt" class="{{Route::current()->getName() == 'view.report' ? 'active' : ''}}"><div class="pull-left"><i class="fa fa-bar-chart-o mr-20" aria-hidden="true"></i><span class="right-nav-text">Report Management </span></div><div class="pull-right"><i class="zmdi zmdi-caret-down"></i></div><div class="clearfix"></div></a>
