747fcaeb0cae75b93ffd1b0c08fcaaf53c5438d2
Jhonnel Rosales <jhonnel@bbo.com.ph>
2019-05-22 10:25:13 +0800

    update

--- app/Http/Controllers/RolePermissionController.php
+++ app/Http/Controllers/RolePermissionController.php
@@ -32,7 +32,7 @@ class RolePermissionController extends Controller
      */
     public function index()
     {
-        return view('admin.roles.index', compact('roles'));
+        return view('admin.roles.index');
     }
 
     public function store(RoleFormRequest $request)
